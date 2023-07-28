9e34ac8cc38dfabc7d46d54256427ffedd9bd6fd
LucasPinottiBrito <lucaspinottibrito@gmail.com>
2023-07-27 23:09:15 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="Header">
+        <img src="" alt="" className="logo" />
+
+      </div>
+      <div className="body">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
